1f10ef8ebdc17ebd8481ccd75b4ee879bb9c86f5
Umar Abdullah <abdullahgilmannn@gmail.com>
2026-09-22 22:35:38 +0530

    feat: setup mongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/database")
+
 const app = express();
 
 app.use(cors());
@@ -16,6 +18,11 @@ app.get("/api/health", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
