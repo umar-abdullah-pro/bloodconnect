21cca256e1ebcac4368198bf0fd54ff416998f84
Umar Abdullah <abdullahgilmannn@gmail.com>
2026-09-23 12:35:22 +0530

    chore: added authorize Roles middleware

---
+++ backend/middlewares/authorizeMiddleware.js
@@ -0,0 +1,14 @@
+const authorizeRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "Access denied",
+      });
+    }
+
+    next();
+  };
+};
+
+module.exports = authorizeRoles;
